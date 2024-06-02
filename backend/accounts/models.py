7e298715bc2e14a0a161ff00cb5a0803af0fd148
CSchoice <tkdlqjf@gmail.com>
2024-06-02 14:01:09 +0900

    Feat: 유저모델 생성

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    # 커스텀 필드 추가
+    birth_date = models.DateField(null=True, blank=True)
+    real_name = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
