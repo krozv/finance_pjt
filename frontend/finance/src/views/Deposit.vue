378e6d88bab4f0b551c998f33ebdab12db24549d
krozv <krozv.code@gmail.com>
2024-06-04 15:39:07 +0900

    Feat: 예금 은행 검색 화면 추가

--- frontend/finance/src/views/Deposit.vue
+++ frontend/finance/src/views/Deposit.vue
@@ -9,10 +9,28 @@
             예금 상품을 찾아보세요.
         </h2>
         <div v-if="showSearch">
-            <v-text-field
-                label="Search"
-                prepend-inner-icon="mdi-magnify"
-            ></v-text-field>
+            <v-card 
+                class="mt-3"
+                variant="outlined"
+                color="white"
+                rounded="lg">
+                <v-card-text>
+                    <h3>🏦은행을 선택하세요.</h3>
+                    <v-chip-group
+                        multiple
+                        column
+                        v-model=selection
+                        selected-class="selected"
+                    >
+                        <v-chip
+                            v-for="bank in banks"
+                            :key="bank"
+                            :text="bank"
+                            variant="outlined"
+                        ></v-chip>
+                    </v-chip-group>
+                </v-card-text>
+            </v-card>
         </div>
         <v-row class="mt-1">
             <v-col cols="4"
@@ -39,19 +57,31 @@ const items = [
     {title: '예금1', bank: '기업은행'},
     {title: '예금1', bank: '기업은행'},
 ]
+
 // 예금 검색 기능 추가 예정
 const showSearch = ref(false)
+
+// 예금 검색 관련 은행 리스트
+const banks = [
+    'NH농협', '신한', '하나', '부산', '대구', 'SC제일', '수협', '제주', '카카오뱅크', 'KDB산업',
+    '국민', '우리'
+]
+const selection = ref(null)
 </script>
 
 <style scoped>
 h2 {
     color: white
 }
-h2:hover{
+h2:hover {
     text-decoration: underline;
 }
 .searchIcon {
     stroke: white;
     stroke-width: 1;
+}
+.selected {
+    background-color: white;
+    color: #3F6387;
 }
 </style>
